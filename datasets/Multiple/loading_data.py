6fc17f4961e1dd5bbca9c4f7db80876a6091d5ff
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-04-03 18:27:42 +0000

    test transforms WE

--- datasets/Multiple/loading_data.py
+++ datasets/Multiple/loading_data.py
@@ -31,7 +31,7 @@ def loading_data():
     train_main_transform_WE = own_transforms.Compose([
         #In progress
         own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
-        own_transforms.ColorJitter(brightness=0.3),
+        #own_transforms.ColorJitter(brightness=0.3),
         #own_transforms.ColorJitter(brightness=0.5),
         own_transforms.RandomHorizontallyFlip()
     ])
