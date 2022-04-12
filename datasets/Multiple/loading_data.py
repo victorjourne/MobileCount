301219b1eb1ca259bec4cb494be6496c3fa96f2b
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-04-12 15:11:16 +0000

    loading data for JHU

--- datasets/Multiple/loading_data.py
+++ datasets/Multiple/loading_data.py
@@ -1,7 +1,9 @@
+import torch
+import numpy as np
+import pandas as pd
+import misc.transforms as own_transforms
 import torchvision.transforms as standard_transforms
 from torch.utils.data import DataLoader
-
-import misc.transforms as own_transforms
 from datasets.Multiple.loader import DynamicDataset, CollateFN
 from datasets.Multiple.settings import cfg_data
 
@@ -11,30 +13,30 @@ def loading_data():
     log_para = cfg_data.LOG_PARA
     cl = CollateFN(cfg_data.TRAIN_SIZE)
     collate = cl.collate if cfg_data.COLLATE_FN and cfg_data.TRAIN_BATCH_SIZE != 1 else None
-
+    
     # Add here specific transform func : 
     # Choose differents combinaison of transformations for each dataset
     train_main_transform_SHHA = own_transforms.Compose([
-        # own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.ColorJitter(brightness=0.5),
         own_transforms.RandomHorizontallyFlip()
     ])
-
+    
     train_main_transform_SHHB = own_transforms.Compose([
-        # own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.ColorJitter(brightness=0.5),
         own_transforms.RandomDownOverSampling(4),
         own_transforms.RandomHorizontallyFlip()
     ])
 
     train_main_transform_WE = own_transforms.Compose([
-        # own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.ColorJitter(brightness=0.5),
         own_transforms.RandomHorizontallyFlip()
     ])
 
     train_main_transform_BG = own_transforms.Compose([
-        # own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.ColorJitter(brightness=0.5),
         own_transforms.RandomHorizontallyFlip()
     ])
@@ -46,17 +48,17 @@ def loading_data():
     ])
 
     train_main_transform_GCC = own_transforms.Compose([
-        # own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.RandomHorizontallyFlip()
     ])
-
-    specific_transform = {"SHHA__transform": train_main_transform_SHHA,
-                          "SHHB__transform": train_main_transform_SHHB,
-                          "GCC__transform": train_main_transform_GCC,
-                          "WE__transform": train_main_transform_WE,
-                          "JHU__transform": train_main_transform_JHU,
-                          "BG__transform": train_main_transform_BG}
-
+    
+    specific_transform = {"SHHA__transform" : train_main_transform_SHHA,
+                          "SHHB__transform" : train_main_transform_SHHB,
+                          "GCC__transform" : train_main_transform_GCC,
+                          "WE__transform" : train_main_transform_WE,
+                          "JHU__transform" : train_main_transform_JHU,
+                          "BG__transform" : train_main_transform_BG}
+    
     if specific_transform:
         cfg_data.PATH_SETTINGS.update(specific_transform)
 
@@ -78,35 +80,38 @@ def loading_data():
         own_transforms.DeNormalize(*mean_std),
         standard_transforms.ToPILImage()
     ])
-
+        
     train_set = DynamicDataset(couple_datasets=cfg_data.LIST_C_DATASETS,
-                               mode='train',
-                               main_transform=train_main_transform,
-                               img_transform=img_transform,
-                               gt_transform=gt_transform,
-                               image_size=cfg_data.IMAGE_SIZE,
-                               **cfg_data.PATH_SETTINGS)
-
-    train_loader = DataLoader(train_set,
-                              batch_size=cfg_data.TRAIN_BATCH_SIZE,
+                              mode='train',
+                              main_transform=train_main_transform,
+                              img_transform=img_transform,
+                              gt_transform=gt_transform, 
+                              image_size=cfg_data.IMAGE_SIZE,
+                              **cfg_data.PATH_SETTINGS)
+    
+    train_loader = DataLoader(train_set, 
+                              batch_size=cfg_data.TRAIN_BATCH_SIZE, 
                               num_workers=8,
                               collate_fn=collate,
-                              shuffle=True,
+                              shuffle=True, 
                               drop_last=True)
-
+    
     val_set = DynamicDataset(couple_datasets=cfg_data.LIST_C_DATASETS,
-                             mode='test',
-                             main_transform=None,
-                             img_transform=img_transform,
-                             gt_transform=gt_transform,
-                             image_size=cfg_data.IMAGE_SIZE,
-                             **cfg_data.PATH_SETTINGS)
+                              mode='test',
+                              main_transform=None,
+                              img_transform=img_transform,
+                              gt_transform=gt_transform,
+                              image_size=cfg_data.IMAGE_SIZE,
+                              **cfg_data.PATH_SETTINGS)
     val_loader = None
-    if len(val_set) > 0:
-        val_loader = DataLoader(val_set,
-                                batch_size=cfg_data.VAL_BATCH_SIZE,
-                                num_workers=8,
-                                shuffle=True,
+    if len(val_set)>0:
+        val_loader = DataLoader(val_set, 
+                                batch_size=cfg_data.VAL_BATCH_SIZE, 
+                                num_workers=8, 
+                                shuffle=True, 
                                 drop_last=False)
 
     return train_loader, val_loader, restore_transform
+
+
+
