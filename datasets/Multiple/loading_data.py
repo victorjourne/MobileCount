2fbc9621a8e4a14bd9adfec47514d0e5beaae3f9
fabricejourdan <jourdan.fabrice@yahoo.fr>
2022-04-14 13:03:20 +0000

    pre gcc

--- datasets/Multiple/loading_data.py
+++ datasets/Multiple/loading_data.py
@@ -42,7 +42,7 @@ def loading_data():
     ])
 
     train_main_transform_GCC = own_transforms.Compose([
-        #own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
         own_transforms.RandomHorizontallyFlip()
     ])
     
